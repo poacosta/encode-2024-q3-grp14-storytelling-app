9c7a80ea49f055eb862cbc214e659985bb390575
poacosta <poacosta87@gmail.com>
2024-09-07 12:13:39 -0600

    Setting components for better mockup management

---
+++ components/title.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+    return <div className="space-y-2">
+        <h2 className="text-3xl font-bold">Story Telling App</h2>
+        <p className="text-zinc-500 dark:text-zinc-400">
+            Customize the story by selecting the genre and tone.
+        </p>
+    </div>
+}
+
+export {Title};
