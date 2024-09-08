2e76e4c337f418e2d7cdb42ac8b5b5309a3d214f
Fadamiro Catherine <34650699+katiie@users.noreply.github.com>
2024-09-08 08:30:02 -0600

    Added the logic about User Character (#2)
    * Added initial code to include user components
    
    * Adjust logic to edit and remove character
    
    ---------
    
    Co-authored-by: Catherine Fadamiro <catherine@catchafire.org>

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import {GenreSelector} from "@/components/genres";
 import {ToneSelector} from "@/components/tones";
 import {StoryField} from "@/components/story-field";
 import {GenerateStoryButton} from "@/components/generate-story-button";
+import {UserCharacter} from "@/components/user-character"
 
 export default function Chat() {
     const {messages, append, isLoading} = useChat();
@@ -14,7 +15,49 @@ export default function Chat() {
     const handleChange = ({target: {name, value}}: React.ChangeEvent<HTMLInputElement>) => {
         setState({...state, [name]: value});
     };
+    const [formData, setFormData] = useState({
+        index: undefined,
+        name: '',
+        description: '',
+        personality: '',
+    });
+    const handleFormChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+    };
+    const [usercharacters, setUserCharacters] = useState([formData]);
 
+    const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        let first_element = usercharacters[0]
+        if (first_element && first_element['name'] === ''){
+            setUserCharacters([])
+        }
+        if (formData['index'] != null){
+            usercharacters[formData['index']] = formData
+            setUserCharacters(usercharacters);
+        } else{
+            setUserCharacters((prevData) =>
+                [...prevData, formData]);
+        }
+        setFormData({ index: undefined, name: '', description: '', personality: '' });
+    };
+
+    const handleDelete = (index: number) => {
+        if (usercharacters.length == 1){
+            setUserCharacters([]);
+        } else{
+            let new_set = usercharacters.splice(index, 1);
+            setUserCharacters(new_set);
+        }
+    }
+    const handleEdit = (index: number) => {
+        let record = usercharacters[index];
+        setFormData(record);
+    }
     return (
         <main className="mx-auto w-full p-24 flex flex-col">
             <div className="p4 m-4">
@@ -22,7 +65,11 @@ export default function Chat() {
                     <Title/>
                     <GenreSelector selectedGenre={state.genre} onChange={handleChange}/>
                     <ToneSelector selectedTone={state.tone} onChange={handleChange}/>
-                    <GenerateStoryButton isLoading={isLoading} state={state} append={append}/>
+                    <UserCharacter formData={formData} handleFormChange={handleFormChange}
+                        handleFormSubmit={handleFormSubmit} usercharacters={usercharacters}
+                        handleDelete={handleDelete} handleEdit={handleEdit} />
+                    <GenerateStoryButton isLoading={isLoading} state={state} append={append}
+                        usercharacters={usercharacters}/>
                     <StoryField messages={messages}/>
                 </div>
             </div>
--- components/generate-story-button.tsx
+++ components/generate-story-button.tsx
@@ -5,15 +5,28 @@ interface GenerateStoryButtonProps {
     isLoading: boolean;
     state: { genre: string; tone: string; };
     append: (message: Message | CreateMessage, chatRequestOptions?: ChatRequestOptions) => Promise<string | null | undefined>;
+    usercharacters : {name: string, description: string, personality: string }[];
 }
 
-const GenerateStoryButton: React.FC<GenerateStoryButtonProps> = ({isLoading, state, append}) => {
+const GenerateStoryButton: React.FC<GenerateStoryButtonProps> = ({isLoading, state, append, usercharacters}) => {
+
     const handleClick = () => {
         const message: CreateMessage = {
             role: "user",
             content: `Generate a story with genre ${state.genre} and tone ${state.tone}`
         };
         append(message);
+        if (usercharacters !== null) {
+            for (const item of usercharacters){
+                console.log(item)
+                let newmessage: CreateMessage = {
+                    role: "user",
+                    content: `Add this character to the story with the name ${item.name},
+                    with these description ${item.description} and personality ${item.personality} `
+                };
+                append(newmessage);
+            }
+        }
     };
 
     return <button
